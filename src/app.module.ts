e44d72b5734e0ff9ab502af81d6545af5374a3e8
Ferenc Almasi <v-falmasi@hotels.com>
2021-03-24 14:16:58 +0100

    ✨ Integrating GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [CatModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        CatModule,
+        GraphQLModule.forRoot({
+            autoSchemaFile: 'schema.gql'
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/cat/cat.module.ts
+++ src/cat/cat.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CatController } from './cat.controller';
 import { CatService } from './cat.service';
 import { CatResolver } from './cat.resolver';
 
 @Module({
-  controllers: [CatController],
-  providers: [CatService, CatResolver]
+    providers: [CatService, CatResolver]
 })
 export class CatModule {}
