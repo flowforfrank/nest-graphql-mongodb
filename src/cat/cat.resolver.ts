030923997a329aba518bd88f1821f175714418c6
Ferenc Almasi <v-falmasi@hotels.com>
2021-03-26 09:29:30 +0100

    ✨ Extending service and resolver

--- src/cat/cat.resolver.ts
+++ src/cat/cat.resolver.ts
@@ -1,9 +1,28 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CatService } from './cat.service';
+import { CatType } from './cat.dto';
 
 @Resolver()
 export class CatResolver {
-    @Query(returns => String)
-    async hello() {
-        return '👋';
+    constructor(private readonly catService: CatService) {}
+
+    @Query(returns => [CatType])
+    async cats() {
+        return this.catService.findAll();
+    }
+
+    @Mutation(returns => CatType)
+    async createCat(@Args('input') input: CatType) {
+        return this.catService.create(input);
+    }
+
+    @Mutation(returns => CatType)
+    async updateCat(@Args('id') id: string, @Args('input') input: CatType) {
+        return this.catService.update(id, input);
+    }
+
+    @Mutation(returns => CatType)
+    async deleteCat(@Args('id') id: string) {
+        return this.catService.delete(id);
     }
 }
--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,4 +1,27 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat, CatDocument } from './cat.schema';
+import { CatType } from './cat.dto';
 
 @Injectable()
-export class CatService {}
+export class CatService {
+    constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
+
+    async findAll(): Promise<Cat[]> {
+        return this.catModel.find().exec();
+    }
+
+    async create(createCatDto: CatType): Promise<Cat> {
+        const createdCat = new this.catModel(createCatDto);
+        return createdCat.save();
+    }
+
+    async update(id: string, updateCatDto: CatType): Promise<Cat> {
+        return this.catModel.findByIdAndUpdate(id, updateCatDto);
+    }
+
+    async delete(id: string): Promise<Cat> {
+        return this.catModel.findByIdAndDelete(id);
+    }
+}
