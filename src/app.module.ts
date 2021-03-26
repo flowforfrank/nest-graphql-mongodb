e0f7010cd713273bbca5f30ba57a658e78452370
Ferenc Almasi <v-falmasi@hotels.com>
2021-03-26 09:29:05 +0100

    ✨ Integrate MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
         CatModule,
         GraphQLModule.forRoot({
             autoSchemaFile: 'schema.gql'
-        })
+        }),
+        MongooseModule.forRoot('mongodb://localhost/nest')
     ],
     controllers: [AppController],
     providers: [AppService],
--- src/cat/cat.module.ts
+++ src/cat/cat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CatService } from './cat.service';
 import { CatResolver } from './cat.resolver';
-
+import { Cat, CatSchema } from './cat.schema';
 @Module({
-    providers: [CatService, CatResolver]
+    imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+    providers: [CatService, CatResolver],
 })
 export class CatModule {}
